323463bb5c3c506032e15be0e7ec169542eeebd4
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-08-01 09:36:11 +0200

    renamed home to header

---
+++ src/views/Header.vue
@@ -0,0 +1,13 @@
+<!-- src/views/Home.vue -->
+<template>
+    <div>
+      <h1>Header goes here</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home'
+  }
+  </script>
+  
