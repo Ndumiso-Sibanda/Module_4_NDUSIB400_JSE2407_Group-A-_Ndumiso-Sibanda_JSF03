710ddede6612ca27c3c5873acbc6036a042f7532
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-08-02 15:25:02 +0200

    feat(product-card): Add product image, title, and rating display with SVG stars

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -22,3 +22,20 @@
               :key="product.id"
               class="flex flex-col max-h-[130rem] cursor-pointer max-w-80 hover:-translate-y-1 hover:scale-105 duration-300 bg-white border border-slate-200 shadow shadow-slate-950/5 rounded-2xl overflow-hidden"
             >
+            <img class="object-contain h-48 mt-3" :src="product.image" :alt="product.title" />
+              <div class="flex-1 flex flex-col p-6">
+                <header class="mb-2 flex-2">
+                  <h2 class="text-lg line-clamp-2 font-extrabold leading-snug text-slate-600">
+                    {{ product.title }}
+                  </h2>
+                </header>
+                <div class="flex items-left -ml-2 mb-2">
+                  <svg
+                    v-for="index in Math.round(product.rating.rate)"
+                    :key="index"
+                    class="w-4 h-4 text-yellow-300 ms-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  ></svg>
