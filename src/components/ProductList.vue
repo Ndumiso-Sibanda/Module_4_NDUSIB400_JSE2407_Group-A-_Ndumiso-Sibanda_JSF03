c1bd3cc2595e6ca26bf5bd1906e74b22da27b234
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-08-02 15:39:15 +0200

    feat(product-list): Complete component with reset filters, add to cart, and mounted lifecycle methods

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -42,7 +42,6 @@
                   <path
                       d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
                     />
-                  </svg>
                   <svg
                     v-for="index in 5 - Math.round(product.rating.rate)"
                     :key="index"
@@ -85,7 +84,6 @@
                           stroke-width="2"
                           d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
                         />
-                      </svg>
                     </button>
                   </div>
                 </div>
@@ -159,3 +157,18 @@ export default {
             this.filteredProducts = [...this.products]; 
             break;
         }
+    },
+      resetFiltersAndSorting() {
+        this.selectedCategory = '';
+        this.sortOrder = 'default';
+        this.filteredProducts = [...this.products];
+      },
+      addToCart(product) {
+        console.log('Add to cart:', product);
+      },
+    },
+    mounted() {
+      this.getProducts();
+    },
+  };
+  </script>
