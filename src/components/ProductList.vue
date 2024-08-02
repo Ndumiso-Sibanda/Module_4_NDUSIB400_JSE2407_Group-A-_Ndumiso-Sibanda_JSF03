210f10f9626b77913851736ea1c6ec719a17736a
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-08-02 15:30:24 +0200

    feat(product-card): Add 'Add to Favourites' button with SVG icon

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -66,3 +66,15 @@
                       {{ product.category }}
                     </span>
                   </div>
+                  <div class="justify-end space-x-2">
+                    <button aria-label="Add to Favourites">
+                      <svg
+                        class="me-1.5 h-5 w-5 hover:fill-red-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        transform="scale(1.6)"
+                      ></svg>
