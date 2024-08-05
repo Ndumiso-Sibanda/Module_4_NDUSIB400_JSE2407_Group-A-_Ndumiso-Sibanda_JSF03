74b24d17a53c0b8a9268c35521677a818e0baca1
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-08-05 14:52:49 +0200

    wrapped my products with route link to ensure routing

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -18,9 +18,10 @@
       <div v-else>
         <div class="grid justify-center">
           <div class="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-4">
-            <div
+            <router-link
               v-for="product in filteredProducts"
               :key="product.id"
+              :to="'/product/' + product.id"
               class="flex flex-col max-h-[130rem] cursor-pointer max-w-80 hover:-translate-y-1 hover:scale-105 duration-300 bg-white border border-slate-200 shadow shadow-slate-950/5 rounded-2xl overflow-hidden"
             >
               <img class="object-contain h-48 mt-3" :src="product.image" :alt="product.title" />
@@ -100,12 +101,13 @@
                   Add To Cart
                 </button>
               </div>
-            </div>
+            </router-link>
           </div>
         </div>
       </div>
     </div>
   </template>
+  
   
   <script>
   export default {
